3fbcdca8de52adb0eb29294bae72ebd10e2e876f
Fabricio <fabriciofs.comp@gmail.com>
2016-10-07 17:06:43 -0300

    express installed, express json response made

--- Projects/task3/hello.js
+++ Projects/task3/hello.js
@@ -1,8 +1,8 @@
 var http = require('http');
-
+var json = JSON.stringify({hello : "from the otherside"});
 var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"content-type" : "text/plain"})
-    response.end ("Hello World\n")
+    response.writeHead(200, {"content-type" : "application/json"});
+    response.end (json);
 });
-
+console.log('Listening on http//:localhost:8000');
 server.listen(8000);
